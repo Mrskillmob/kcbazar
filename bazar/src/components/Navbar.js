6982bf8aa8ad7c90f3733f28a562d45053298614
Martin Kubica <martinko29.km@gmail.com>
2023-04-22 19:06:41 +0200

    linky
    linky

--- bazar/src/components/Navbar.js
+++ bazar/src/components/Navbar.js
@@ -5,11 +5,11 @@ export default function Navbar() {
   return (
     <div className="flex flex-row w-full p-2 bg-navbar items-center">
       <em className="ml-2 text-lime-500 font-bold text-2xl text-purple"> KC Bazár</em>
-      <Link to="/" className="ml-2 text-white font-bold text-l pl-10">Domov</Link>
-      <Link to="/allItems" className="ml-1 text-white font-bold text-l pl-10  ">Ponuka</Link>
-      <Link to="/add" className="ml-2 text-white font-bold text-l pl-10">Predaj</Link>
-      <Link to="/about" className="ml-2 text-white font-bold text-l pl-10">O nás</Link>
-      <Link to="/login" className="ml-2 text-white font-bold text-l pl-10">Prihlásenie</Link>
+      <Link to="/kcbazar/" className="ml-2 text-white font-bold text-l pl-10">Domov</Link>
+      <Link to="/kcbazar/allItems" className="ml-1 text-white font-bold text-l pl-10  ">Ponuka</Link>
+      <Link to="/kcbazar/add" className="ml-2 text-white font-bold text-l pl-10">Predaj</Link>
+      <Link to="/kcbazar/about" className="ml-2 text-white font-bold text-l pl-10">O nás</Link>
+      <Link to="/kcbazar/login" className="ml-2 text-white font-bold text-l pl-10">Prihlásenie</Link>
     </div>
   )
 }
