ca665803843fb7051f68a8c4cc7a525c623385fa
Martin Kubica <martinko29.km@gmail.com>
2023-04-23 17:03:01 +0200

    test
    test

--- bazar/src/components/Footer.js
+++ bazar/src/components/Footer.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function footer() {
   return (
-    <div className="bg-navbar flex justify-center flex-row items-center w-full">
-      <h1 className="text-white">Made with ❤ by Martin & Simon</h1>
+    
+    <div className="bg-navbar flex justify-center flex-row items-center w-full border-t-8 border-white">
+        <h1 className="text-white">Made with ❤ by Martin & Simon</h1>
     </div>
   )
 }
--- bazar/src/components/Navbar.js
+++ bazar/src/components/Navbar.js
@@ -3,13 +3,13 @@ import {Link} from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div className="flex flex-row w-full p-2 bg-navbar items-center">
+    <div className="flex flex-row w-full p-2 bg-navbar items-center border-b-8 border-white border-solid">
       <em className="ml-2 text-lime-500 font-bold text-2xl text-purple"> KC Bazár</em>
-      <Link to="/kcbazar/" className="ml-2 text-white font-bold text-l pl-10">Domov</Link>
-      <Link to="/kcbazar/allItems" className="ml-1 text-white font-bold text-l pl-10  ">Ponuka</Link>
-      <Link to="/kcbazar/add" className="ml-2 text-white font-bold text-l pl-10">Predaj</Link>
-      <Link to="/kcbazar/about" className="ml-2 text-white font-bold text-l pl-10">O nás</Link>
-      <Link to="/kcbazar/login" className="ml-2 text-white font-bold text-l pl-10">Prihlásenie</Link>
+      <Link to="/kcbazar/" className="ml-2 text-white font-bold text-xl pl-10">Domov</Link>
+      <Link to="/kcbazar/allItems" className="ml-2 text-white font-bold text-xl pl-10">Ponuka</Link>
+      <Link to="/kcbazar/add" className="ml-2 text-white font-bold text-xl pl-10">Predaj</Link>
+      <Link to="/kcbazar/about" className="ml-2 text-white font-bold text-xl pl-10">O nás</Link>
+      <Link to="/kcbazar/login" className="ml-2 text-white font-bold text-xl pl-10">Prihlásenie</Link>
     </div>
   )
 }
